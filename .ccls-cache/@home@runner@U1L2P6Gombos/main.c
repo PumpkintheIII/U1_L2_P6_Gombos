35163686c7e8167a814c3e02e1143578b061aaad
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-06 13:31:00 +0000

    Iteration 4

--- .ccls-cache/@home@runner@U1L2P6Gombos/main.c
+++ .ccls-cache/@home@runner@U1L2P6Gombos/main.c
@@ -13,13 +13,15 @@ int main(void) { //defines main function
   int perimeterLen, perimeterWidth, perimeterInch, perimeterFoot;
   //initialize variables as integer
 
-  printf("Please enter the length in feet: "); //asks user for length
+  printf("This program allows you to find the area and perimeter of a rectangle from an entered length and width. You will get four different prompts to input the lenght and width of the rectangle in whole numbers: \n\t- Length in feet: Input your length in feet\n\t- Lenght in inches: Input the remainder of your length in inches\n\t- Width in feet: Input your width in feet\n\t- Width in inches: Input the remainder of your width in inches\n"); //instructions
+  
+  printf("Length in feet: "); //asks user for length
   scanf("%d", &lenFoot); //accepts user input and formats as integer
-  printf("Please enter the length in inches: "); //asks user for length
+  printf("Length in inches: "); //asks user for length
   scanf("%d", &lenInch); //accepts user input and formats as integer
-  printf("Please enter the width in feet: "); //asks user for width
+  printf("Width in feet: "); //asks user for width
   scanf("%d", &widFoot); //accepts user input and formats as integer
-  printf("Please enter the width in inches: "); //asks user for width
+  printf("Width in inches: "); //asks user for width
   scanf("%d", &widInch); //accepts user input and formats as integer
 
   lenTotal = lenFoot*12; //converts length from feet to inches
@@ -27,21 +29,21 @@ int main(void) { //defines main function
   widTotal = widFoot*12; //converts width from feet to inches
   widTotal += widInch; //adds width inches
 
-  printf("Lenght in inchs: %d", lenTotal); //prints lenght
-  printf("\nWidth in inches: %d", widTotal); //prints width
-
   //Perimeter:
-  perimeterLen = lenTotal*2;
-  perimeterWidth = widTotal*2;
+  perimeterLen = lenTotal*2; //multiplies the length by 2
+  perimeterWidth = widTotal*2; //multiplies the width by 2
   perimeterInch = perimeterLen + perimeterWidth;
+  //finds the perimeter by adding the doubled lenght and width
   perimeterFoot = perimeterInch/12;
   perimeterInch = perimeterInch - (perimeterFoot*12);
+  //converts inches to feet and inches
   printf("\nPerimeter: %d\'%d\"", perimeterFoot, perimeterInch);
+  //prints perimeter
 
   //Area:
   areaFoot = (lenTotal/12) * (widTotal/12);
-  areaInch = areaInch - (areaFoot*12);
-  printf("\nArea: %f\'", areaFoot);
+  //converts length and width to feet and multiplies them by each other to find the area
+  printf("\nArea: %f\'", areaFoot); //prints the area
   
   return 0;
 }
